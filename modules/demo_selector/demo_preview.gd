4b6ec25a681865ece1f2b7e2e4e2d23474d2a506
antimundo <64472605+antimundo@users.noreply.github.com>
2025-05-27 12:37:28 +0200

    Fix hover in demo selector

--- modules/demo_selector/demo_preview.gd
+++ modules/demo_selector/demo_preview.gd
@@ -2,23 +2,29 @@ class_name DemoPreview
 extends Control
 
 @export var data: DemoData
+var _hover_tween: Tween
 
 func _ready() -> void:
 	%PreviewImage.texture = data.image
 	%DemoTitle.text = data.title
+	%Hover.modulate = Color.TRANSPARENT
 
 func _on_pressed() -> void:
 	print("click")
 
 func _on_mouse_entered() -> void:
+	if _hover_tween != null:
+		_hover_tween.kill()
 	var hover: Control = %Hover
-	hover.modulate = Color.TRANSPARENT
 	hover.visible = true
-	create_tween().tween_property(hover, "modulate", Color.WHITE, .25)
+	_hover_tween = create_tween()
+	_hover_tween.tween_property(hover, "modulate", Color.WHITE, .2)
 
 func _on_mouse_exited() -> void:
+	if _hover_tween != null:
+		_hover_tween.kill()
 	var hover: Control = %Hover
-	var tween: Tween = create_tween()
-	tween.tween_property(hover, "modulate", Color.TRANSPARENT, .25)
-	await tween.finished
+	_hover_tween = create_tween()
+	_hover_tween.tween_property(hover, "modulate", Color.TRANSPARENT, .2)
+	await _hover_tween.finished
 	hover.visible = false

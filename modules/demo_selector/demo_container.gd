30282ea548a0e64f9c67501cd44e108da1613605
antimundo <64472605+antimundo@users.noreply.github.com>
2025-05-27 12:23:20 +0200

    Update demo list layout

--- modules/demo_selector/demo_container.gd
+++ modules/demo_selector/demo_container.gd
@@ -1,6 +1,6 @@
 extends GridContainer
 
-var demo_list_resource: DemoList = load("res://modules/demo_selector/demo_list.tres")
+@export var demo_list_resource: DemoList
 var demo_preview: PackedScene = load("res://modules/demo_selector/demo_preview.tscn")
 
 func _ready() -> void:
--- modules/demo_selector/demo_preview.gd
+++ modules/demo_selector/demo_preview.gd
@@ -4,19 +4,20 @@ extends Control
 @export var data: DemoData
 
 func _ready() -> void:
-	$PreviewImage.texture = data.image
+	%PreviewImage.texture = data.image
+	%DemoTitle.text = data.title
 
 func _on_pressed() -> void:
 	print("click")
 
 func _on_mouse_entered() -> void:
-	var hover: Control = $Hover
+	var hover: Control = %Hover
 	hover.modulate = Color.TRANSPARENT
 	hover.visible = true
 	create_tween().tween_property(hover, "modulate", Color.WHITE, .25)
 
 func _on_mouse_exited() -> void:
-	var hover: Control = $Hover
+	var hover: Control = %Hover
 	var tween: Tween = create_tween()
 	tween.tween_property(hover, "modulate", Color.TRANSPARENT, .25)
 	await tween.finished

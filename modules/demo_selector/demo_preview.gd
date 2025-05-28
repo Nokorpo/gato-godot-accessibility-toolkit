d584cdaf07db82596655fd41b2f38db3b6fbecfd
Igg2000 <ignaciogarcia200@gmail.com>
2025-05-28 11:23:41 +0200

    Add scale transition on focus in Demo Preview

--- modules/demo_selector/demo_preview.gd
+++ modules/demo_selector/demo_preview.gd
@@ -40,13 +40,15 @@ func _show_hover() -> void:
 	var hover: Control = %Hover
 	hover.visible = true
 	_hover_tween = create_tween()
-	_hover_tween.tween_property(hover, "modulate", Color.WHITE, .2)
+	_hover_tween.parallel().tween_property(hover, "modulate", Color.WHITE, .2)
+	_hover_tween.parallel().tween_property(self, "scale", Vector2.ONE * 1.15, .2)
 
 func _hide_hover() -> void:
 	if _hover_tween != null:
 		_hover_tween.kill()
 	var hover: Control = %Hover
 	_hover_tween = create_tween()
-	_hover_tween.tween_property(hover, "modulate", Color.TRANSPARENT, .2)
+	_hover_tween.parallel().tween_property(hover, "modulate", Color.TRANSPARENT, .2)
+	_hover_tween.parallel().tween_property(self, "scale", Vector2.ONE, .2)
 	await _hover_tween.finished
 	hover.visible = false

49d4c9bf5ef0fb90e9a3b43ec2842d986b9188dc
Igg2000 <ignaciogarcia200@gmail.com>
2025-06-16 11:07:57 +0200

    Add vfx when you pick a acorn

--- modules/acorn/acorn.gd
+++ modules/acorn/acorn.gd
@@ -12,6 +12,11 @@ func _process(delta: float) -> void:
 		global_position = global_position.move_toward(target_position, follow_speed * delta)
 
 func follow_the_player() -> void:
+	var tween = create_tween()
+	tween.parallel().tween_property($MeshInstance3D,"scale", Vector3(), .01)
+	await tween.finished
 	freeze = true
 	following_player = true
 	position_in_the_row = player.get_item_carried_count() + 1
+	tween = create_tween()
+	tween.parallel().tween_property($MeshInstance3D,"scale", Vector3.ONE, .4)

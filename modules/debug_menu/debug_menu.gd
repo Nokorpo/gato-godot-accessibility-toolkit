59969e56b61392ce0e829432d90fe2f80c7f696b
Edearth <nepormoseno1992@gmail.com>
2025-07-05 01:10:42 +0200

    Add infinite jump, fix debug menu references

---
+++ modules/debug_menu/debug_menu.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var enable_infinite_jumps: bool = false
+
+func toggle_infinite_jumps(toggle: bool) -> void:
+	enable_infinite_jumps = toggle
--- modules/debug_menu/debug_ui.gd
+++ modules/debug_menu/debug_ui.gd
@@ -2,12 +2,10 @@
 extends CanvasLayer
 
 func _ready() -> void:
-	$Panel/MarginContainer/HBoxContainer/HBoxContainer/Version \
-		.set_text("VERSION: " + _get_current_version())
-	$"../Version".set_text(_get_current_version())
+	%Version.set_text(_get_current_version())
 
 func _process(_delta: float) -> void:
-	%FPSCounter \
+	%FPSCounterValue \
 		.set_text("FPS: " + str(Engine.get_frames_per_second()))
 
 func _input(event: InputEvent) -> void:
@@ -16,7 +14,7 @@ func _input(event: InputEvent) -> void:
 
 func _on_game_speed_slider_value_changed(value: float) -> void:
 	Engine.time_scale = value
-	$Panel/MarginContainer/HBoxContainer/GameSpeed/Value.text = "%1.2f" % value
+	%GameSpeedValue.text = "%1.2f" % value
 
 func _on_toggle_bw_filter_pressed():
 	$ColorRect.visible = !$ColorRect.visible

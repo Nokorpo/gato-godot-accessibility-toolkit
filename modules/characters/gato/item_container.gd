6c861ac8804a7b777a85ea5564c20582953e4f91
Edearth <nepormoseno1992@gmail.com>
2025-07-10 10:41:42 +0200

    Move item detection script to scene root

--- modules/characters/gato/item_container.gd
+++ modules/characters/gato/item_container.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends Area3D
 ## This node detects when an Acorn enters its detection area, then makes it follow it.[br][br]
 ## This is used by the player character so that it can gather acorns and have them following it around.
 
@@ -13,18 +13,20 @@ signal item_collected(item: Node3D)
 ## The speed at which the items follow the container.
 @export var item_follow_speed: float = 4.0
 
+@onready var item_container: Node3D = $ItemContainer
+
 var items: Array[Node3D] = []
-var items_floating_toward_container: Array[Node3D] = []
+var _items_floating_toward_container: Array[Node3D] = []
 
 func _physics_process(delta: float) -> void:
 	for i in range(items.size()):
-		var diff := global_position - items[i].global_position
+		var diff := item_container.global_position - items[i].global_position
 		var expected_distance: float = (i+1) * item_separation
 		if diff.length() >= expected_distance:
 			var direction := diff.normalized()
 			items[i].global_position = lerp(
 				items[i].global_position,
-				global_position - direction * expected_distance,
+				item_container.global_position - direction * expected_distance,
 				DebugMenu.adjust_to_animation_speed(item_follow_speed * delta))
 
 ## Makes the collected item go through other items so the player isn't blocked by it
@@ -36,16 +38,16 @@ func _disable_item_physics(item: PhysicsBody3D) -> void:
 
 ## Plays a short animation that moves the collected item behind the container
 func _float_item_toward_container(item: Node3D) -> Tween:
-	items_floating_toward_container.append(item)
+	_items_floating_toward_container.append(item)
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_QUAD)
-	tween.tween_property(item, "global_position", global_position - global_basis.z.normalized() * items.size() * item_separation, .5 * DebugMenu.animation_speed)
-	tween.tween_callback(func(): items_floating_toward_container.erase(item))
+	tween.tween_property(item, "global_position", item_container.global_position - item_container.global_basis.z.normalized() * items.size() * item_separation, .5 * DebugMenu.animation_speed)
+	tween.tween_callback(_items_floating_toward_container.erase.bind(item))
 	return tween
 
-func _on_item_detection_area_body_entered(body: Node3D) -> void:
+func _on_body_entered(body: Node3D) -> void:
 	if body is Acorn:
-		if body in items or body in items_floating_toward_container:
+		if body in items or body in _items_floating_toward_container:
 			# do nothing if item is already detected
 			return
 

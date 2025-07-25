9e57fab337dd8505594ce25e7bd7b4d5cb11a861
Edearth <nepormoseno1992@gmail.com>
2025-07-25 20:50:36 +0200

    Blend boar animation change

--- modules/characters/boar/mesh/boar_mesh.gd
+++ modules/characters/boar/mesh/boar_mesh.gd
@@ -10,7 +10,7 @@ enum Animations {
 }
 
 func play_animation(animation: Animations) -> void:
-	animation_player.play(_map_animation_enum_to_stringname(animation))
+	animation_player.play(_map_animation_enum_to_stringname(animation), 0.35)
 
 func _map_animation_enum_to_stringname(animation: Animations) -> StringName:
 	return Animations.keys()[animation].to_lower()

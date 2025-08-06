b4780943ac1ca363fdf470893a8c0ff84ab5439f
Edearth <nepormoseno1992@gmail.com>
2025-08-06 16:40:52 +0200

    Add example resource for dialogue system

--- modules/core_systems/dialogue_system/tests/test_model.gd
+++ modules/core_systems/dialogue_system/tests/test_model.gd
@@ -33,3 +33,10 @@ class TestResources extends GutTest:
 		container.messages = [create_message(), create_message(), create_message()]
 
 		assert_eq(container.get_all_meshes().size(), 1, "multiple meshes show up when only one is used across multiple meshes")
+
+	func test_load_messages() -> void:
+		var container: DialogueContainer = load("res://modules/core_systems/dialogue_system/tests/test_dialogue.tres")
+		assert_eq(container.messages[0].message, "Hi there!")
+		assert_eq(container.messages[1].message, "This is a test dialogue.")
+		assert_eq(container.messages[2].message, "That's it. Good day.")
+		assert_eq(container.get_all_meshes().size(), 1, "multiple meshes show up when only one is used across multiple meshes")

352e034f48b2ef1875e4cbd66313db3733cd27d6
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2025-08-12 20:39:28 +0200

    Add walk dust particles

--- modules/characters/gato/walk_state.gd
+++ modules/characters/gato/walk_state.gd
@@ -2,6 +2,7 @@ class_name GatoWalkState
 extends StateMachineState
 
 @export var mesh: GatoMesh
+@export var walk_partricles: GPUParticles3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +11,11 @@ func _ready() -> void:
 # Called when the state machine changes to this state.
 func _on_enter_state() -> void:
 	mesh.play_animation(GatoMesh.Animations.WALK)
+	walk_partricles.emitting = true
 
 # Called when the state machine changes from this state to another one.
 func _on_exit_state() -> void:
-	pass # Replace with function body.
+	walk_partricles.emitting = false
 
 # Called every time the state machine's tick() method is called.
 func tick() -> void:

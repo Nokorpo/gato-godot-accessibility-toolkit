edd4559c77178aa217d54d072bcd5d05a12557b8
Edearth <nepormoseno1992@gmail.com>
2025-08-20 17:22:11 +0200

    Create random timer for character blinking

---
+++ modules/characters/random_timer/README.md
@@ -0,0 +1,6 @@
+## Random Timer
+
+This is a custom Timer node that fires its timeout signal at a random interval.
+
+You can set it up by creating a new instance and defining its `minimum_wait_time`
+and `maximum_wait_time`. The parent Timer's `wait_time` property will be overwritten.
---
+++ modules/characters/random_timer/random_timer.gd
@@ -0,0 +1,12 @@
+class_name RandomTimer
+extends Timer
+
+@export var minimum_wait_time: float
+@export var maximum_wait_time: float
+
+func _ready() -> void:
+	timeout.connect(_set_next_random_wait_time)
+	wait_time = randf_range(minimum_wait_time, maximum_wait_time)
+
+func _set_next_random_wait_time() -> void:
+	wait_time = randf_range(minimum_wait_time, maximum_wait_time)

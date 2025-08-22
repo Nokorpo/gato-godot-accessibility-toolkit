5081ceb9230902a8877fcdf950f6a10c9bb27db0
Edearth <nepormoseno1992@gmail.com>
2025-08-22 11:39:31 +0200

    Fix DialogueTrigger smoke test, check area exists

--- modules/core_systems/dialogue_system/controller/dialogue_trigger.gd
+++ modules/core_systems/dialogue_system/controller/dialogue_trigger.gd
@@ -1,11 +1,18 @@
+@tool
 extends Node
 
 @export var trigger_area: Area3D
 @export var dialogue: DialogueContainer
 
 func _ready() -> void:
-	trigger_area.body_entered.connect(_on_body_entered)
+	if trigger_area != null:
+		trigger_area.body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Gato:
 		DialogueSystem.load_dialogue(dialogue)
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if trigger_area == null:
+		return ["The 'trigger_area' variable is not set. DialogueTrigger expects an Area3D node as its 'trigger_area', please, set one up or it won't work. "]
+	return []

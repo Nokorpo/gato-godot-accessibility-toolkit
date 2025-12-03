1426f6b4c33ddd220abe7147240bf7f6df2d0984
Edearth <nepormoseno1992@gmail.com>
2025-12-03 23:48:24 +0100

    Make visibility key configurable in external file

--- addons/gato_screen_filters/controller/ui_injector_service.gd
+++ addons/gato_screen_filters/controller/ui_injector_service.gd
@@ -7,5 +7,11 @@ var ui: CanvasLayer = null
 
 func _ready() -> void:
 	ui = (load(UI_SCENE_UID) as PackedScene).instantiate()
-	ui.keycode_to_toggle_visibility = OS.find_keycode_from_string("F6")
+	ui.keycode_to_toggle_visibility = OS.find_keycode_from_string(get_key_string_from_config())
 	get_tree().root.add_child.call_deferred(ui)
+
+# TODO extract to model folder
+func get_key_string_from_config() -> String:
+	var config := ConfigFile.new()
+	config.load("res://addons/gato_screen_filters/plugin.cfg")
+	return config.get_value("default_variables", "key_to_toggle_ui_visibility", "F6")

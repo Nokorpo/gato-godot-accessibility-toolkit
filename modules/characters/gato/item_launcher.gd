a23f88f756bd4446cf36a0eebd1603a6ee075e0c
Edearth <nepormoseno1992@gmail.com>
2026-02-26 14:28:22 +0100

    Fix item launcher formula to consider height
    We had an issue where the acorn would fall out of a platform when the
    player was slightly above the target platform. This was caused by the
    item_launcher.gd launch velocity formula not considering the difference
    in height (between player and target).

--- modules/characters/gato/item_launcher.gd
+++ modules/characters/gato/item_launcher.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const GRAVITY := 9.8
+
 @export var distance_on_land: float = 3
 @export var height_at_peak: float = 2
 @export var time_to_land: float = 1
@@ -18,12 +20,24 @@ func _launch_item() -> void:
 		acorn.global_transform = global_transform
 		var landing_spot = _get_item_landing_spots()
 		var horizontal_velocity: Vector3
+		var vertical_velocity: float
+
 		if landing_spot:
+			# Since the flight time will change depending on how long the item needs to fall, we
+			# calculate ascent and fall time separately. Then we use that total flight time to get
+			# the horizontal velocity to reach the target at the correct time.
+			var ascent_time := time_to_land / 2
+			var fall_height: float = global_position.y + height_at_peak - landing_spot.y
+			var descent_time: float = sqrt((2 * fall_height) / GRAVITY)
+			var flight_time: float = ascent_time + descent_time
+			vertical_velocity = (0.5 * GRAVITY * flight_time ** 2) / flight_time
+
 			var distance: Vector3 = landing_spot - global_position
-			horizontal_velocity = distance.normalized() * distance.length()/time_to_land
+			horizontal_velocity = distance/flight_time
 		else:
 			horizontal_velocity = acorn.global_basis.z.normalized() * distance_on_land/time_to_land
-		var vertical_velocity: float = 2*(height_at_peak-position.y)+0.25*9.8
+			vertical_velocity = height_at_peak + (0.5 * GRAVITY * time_to_land ** 2) / time_to_land
+
 		acorn.linear_velocity = Vector3(horizontal_velocity.x, vertical_velocity, horizontal_velocity.z)
 
 ## If there are any landing spots for a button in the landing spot detector area, it finds them. Returns a Vector3 or null.

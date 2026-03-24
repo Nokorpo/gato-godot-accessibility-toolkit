ae5ff4402c5b7509547488cabfb8ab062cf22fb7
Edearth <nepormoseno1992@gmail.com>
2026-03-24 15:37:57 +0100

    Implement navrow for control scheme selector

--- addons/input_remapper/ui/control_scheme_selector.gd
+++ addons/input_remapper/ui/control_scheme_selector.gd
@@ -1,13 +1,20 @@
 extends Container
 
-@onready var current_scheme_label: Label = $HBoxContainer/HBoxContainer/CurrentScheme
-@onready var next_scheme_label: Label = $HBoxContainer/NextScheme
-@onready var second_next_scheme_label: Label = $HBoxContainer/NextScheme2
+@onready var current_scheme_label: Label = $RowNavigationContainer/HBoxContainer/HBoxContainer/CurrentScheme
+@onready var next_scheme_label: Label = $RowNavigationContainer/HBoxContainer/NextScheme
+@onready var second_next_scheme_label: Label = $RowNavigationContainer/HBoxContainer/NextScheme2
 
 func _update_ui(scheme_list: Array[GatoControlScheme], current_scheme: int) -> void:
 	current_scheme_label.text = scheme_list[current_scheme].name
 	next_scheme_label.text = scheme_list[(current_scheme + 1) % scheme_list.size()].name
 	second_next_scheme_label.text = scheme_list[(current_scheme + 2) % scheme_list.size()].name
+
+func _on_row_navigation_input(event: InputEvent) -> void:
+	if event.is_pressed():
+		if event.is_action("ui_right"):
+			_load_next_scheme()
+		elif event.is_action("ui_left"):
+			_load_last_scheme()
 
 func _load_next_scheme() -> void:
 	InputRemapper.load_next_scheme()

400108f57740369a2715b5df94ddc5f17f81e75d
Edearth <nepormoseno1992@gmail.com>
2026-03-24 16:32:14 +0100

    Avoid setting controls to special keys

--- addons/input_remapper/ui/press_key_dialog.gd
+++ addons/input_remapper/ui/press_key_dialog.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const EXCLUDED_KEYS := [KEY_ENTER, KEY_ESCAPE, KEY_META, KEY_CTRL, KEY_SHIFT, KEY_ALT]
+
 signal input_read(input: InputEventKey)
 
 var reading_input: bool = false
@@ -12,6 +14,8 @@ func _input(event: InputEvent) -> void:
 	if not reading_input:
 		return
 	if not event.is_pressed():
+		return
+	if event is InputEventKey and event.keycode in EXCLUDED_KEYS:
 		return
 	if event is InputEventKey or event is InputEventJoypadButton:
 		input_read.emit(event)

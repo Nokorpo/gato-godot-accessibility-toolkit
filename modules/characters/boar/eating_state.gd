5d3249af155a47dd3a62d13dc035ca905adf37da
Edearth <nepormoseno1992@gmail.com>
2026-04-02 17:21:07 +0200

    Fix Jolt warning when scaling acorn to 0

--- modules/characters/boar/eating_state.gd
+++ modules/characters/boar/eating_state.gd
@@ -22,9 +22,12 @@ func _on_enter_state() -> void:
 
 #TODO move to acorn code
 func animate_acorn_disappearance():
+	# We use this vector instead of Vector3.ZERO because setting a scale of
+	# zero is not supported by Jolt Physics and a warning is thrown.
+	const APPROX_ZERO: Vector3 = Vector3(0.00001, 0.00001, 0.00001)
 	var tween := create_tween()
 	tween.set_ease(Tween.EASE_IN)
-	tween.tween_property(target, "scale", Vector3.ZERO, .2)
+	tween.tween_property(target, "scale", APPROX_ZERO, .2)
 	tween.tween_callback(target.queue_free)
 	await tween.finished
 

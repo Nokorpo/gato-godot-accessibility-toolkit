8b188dfdbf7302ed854efd2fa0d82e4c98573f3b
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2026-04-09 11:46:13 +0200

    Remove unneeded method

--- modules/demo5/item_pool/throwable_item_pool.gd
+++ modules/demo5/item_pool/throwable_item_pool.gd
@@ -11,15 +11,12 @@ func _ready():
 	_throwable_items = non_accessible_throwable_items
 
 func spawn_item() -> Node:
-	current_item = _get_random_item()
+	var _random_item: ThrowableItem = _throwable_items.pick_random()
+	current_item = _random_item
 	var spawned_item : Node = current_item.instantiate_item()
 	add_child(spawned_item)
 	spawned_item.position = current_item.spawn_point
 	return spawned_item
-
-func _get_random_item() -> ThrowableItem:
-	var _random_item: ThrowableItem = _throwable_items.pick_random()
-	return _random_item
 
 func swap_items():
 	_throwable_items = accessible_throwable_items

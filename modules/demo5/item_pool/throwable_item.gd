45c6f9ce8139a48af6de506c4ee181c0bc40fa48
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2026-04-09 11:45:11 +0200

    Remove animation player method

--- modules/demo5/item_pool/throwable_item.gd
+++ modules/demo5/item_pool/throwable_item.gd
@@ -13,10 +13,5 @@ func instantiate_item() -> Node:
 	item_node = item_scene.instantiate()
 	return item_node
 
-func get_animator() -> AnimationPlayer:
-	var anim_player : AnimationPlayer
-	anim_player = item_node.get_node("AnimationPlayer")
-	return anim_player
-
 func remove_item():
 	item_node.queue_free()

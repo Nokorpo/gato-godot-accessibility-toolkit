d6e8f139f51f01dc94811172c6010b035cc9d646
Alicia Redondo <149271111+aliciaredmarr@users.noreply.github.com>
2026-05-11 12:40:34 +0200

    Add target selection feedback

--- modules/demo5/targets/bin.gd
+++ modules/demo5/targets/bin.gd
@@ -1,9 +1,11 @@
-extends Node
+extends Node3D
 
 var color: String = "BIN"
 
 @onready var player = %Player
+@onready var outline_material: StandardMaterial3D = load("res://modules/demo5/targets/boars/selection_outline.tres")
 
+var bin_meshes: Array = [MeshInstance3D]
 var anim_player: AnimationPlayer
 var is_open: bool = false
 
@@ -11,6 +13,7 @@ func _ready():
 	anim_player = get_node("AnimationPlayer")
 	player.connect("matched_color", _receive_item)
 	player.connect("focused_target", _focus)
+	bin_meshes = find_children("", "MeshInstance3D", true, false)
 
 func _receive_item(match_color):
 	if match_color == color:
@@ -20,6 +23,18 @@ func _focus(match_color, has_focus):
 	if has_focus and match_color == color:
 		is_open = true
 		anim_player.play("idle_selected")
+		scale *= 1.2
+		_add_outline_material()
 	if !has_focus and is_open:
 		anim_player.play("close_lid")
 		is_open = false
+		scale = Vector3(1,1,1)
+		_remove_outline_material()
+
+func _add_outline_material():
+	for mesh in bin_meshes:
+		mesh.get_active_material(0).next_pass = outline_material
+
+func _remove_outline_material():
+	for mesh in bin_meshes:
+		mesh.get_active_material(0).next_pass = null
--- modules/demo5/targets/boar_demo5.gd
+++ modules/demo5/targets/boar_demo5.gd
@@ -1,23 +1,51 @@
-extends Node
+extends Node3D
 
 @export_enum("BLUE","RED","GREEN","YELLOW") var color: String
 
 @onready var player: Node = %Player
+@onready var outline_material: StandardMaterial3D = load("res://modules/demo5/targets/boars/selection_outline.tres")
 
+var body_material: StandardMaterial3D 
 var anim_player: AnimationPlayer
+var heart_particles: GPUParticles3D
+var fed: bool = false
 
 func _ready():
 	player.connect("matched_color", eat_acorn)
 	player.connect("mismatched_color", refuse_acorn)
+	player.connect("focused_target", focus_feedback)
 	anim_player = get_node("AnimationPlayer")
+	heart_particles = get_node("HeartParticles")
+	body_material = find_child("Body", true, false).get_active_material(0)
 	anim_player.play("idle")
 
 func eat_acorn(matching_color):
 	if matching_color == color:
 		anim_player.animation_set_next("eat", "idle")
 		anim_player.play("eat")
+		if !fed:
+			await get_tree().create_timer(0.3).timeout
+			scale *= 1.2
+			fed = true
+			heart_particles.emitting = true
 
-func refuse_acorn(matching_color, ):
+func refuse_acorn(matching_color):
 	if matching_color == color:
 		anim_player.animation_set_next("refuse", "idle")
 		anim_player.play("refuse")
+		if fed:
+			fed = false
+			heart_particles.emitting = false
+			await get_tree().create_timer(0.3).timeout
+			scale = Vector3(1,1,1)
+
+func focus_feedback(matching_color, focused):
+	if matching_color == color and focused:
+		scale *= 1.3
+		body_material.next_pass = outline_material
+	elif fed:
+		scale = Vector3(1.2,1.2,1.2)
+	else:
+		scale = Vector3(1,1,1)
+	if !focused:
+		body_material.next_pass = null

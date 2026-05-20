4023a4b8339a78e2c5b44ed1c00586fee9f290c4
Edearth <nepormoseno1992@gmail.com>
2026-05-20 17:17:55 +0200

    Add a shadow to characters and some props

--- modules/demo5/item_pool/throwable_item.gd
+++ modules/demo5/item_pool/throwable_item.gd
@@ -6,10 +6,19 @@ extends Resource
 @export var spawn_point: Vector3
 @export_enum("BLUE","RED","GREEN","YELLOW","BIN") var matching_color: String
 
+static var shadow_texture: Texture = preload("res://modules/characters/shadow.svg")
+
 var item_node: Node
 
 func instantiate_item() -> Node:
 	item_node = item_scene.instantiate()
+
+	var shadow := Decal.new()
+	shadow.texture_albedo = shadow_texture
+	shadow.size = Vector3(.20, 5., .20)
+	item_node.add_child(shadow)
+	shadow.position.y -= 2.5
+
 	return item_node
 
 func remove_item():

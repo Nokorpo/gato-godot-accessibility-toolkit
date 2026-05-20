dc268a87314d4260d9658825c98517ba2ca00563
Edearth <nepormoseno1992@gmail.com>
2026-05-20 14:47:01 +0200

    Implement exit with unsaved changes dialog

--- modules/core_systems/game_menu/game_menu.gd
+++ modules/core_systems/game_menu/game_menu.gd
@@ -11,9 +11,10 @@ signal closed
 
 var _settings_menu: Node
 
-func _input(event: InputEvent) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("game_menu") and event.is_pressed():
 		_toggle_visibility()
+		get_viewport().set_input_as_handled()
 
 func _toggle_visibility() -> void:
 	if visible:

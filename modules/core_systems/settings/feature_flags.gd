0f43efe6dbba9f474db3cf0d9077ad1da8e0e541
Edearth <nepormoseno1992@gmail.com>
2026-06-01 14:23:45 +0200

    Feature flag for demo 2
    Repurpose old feature flags config to be used as proper feature flags.
    In this case, we will use it to disable a specific demo for public
    builds. We're under a time pressure to finish it and we'll have to work
    on a version that we aren't comfortable releasing to the public, because
    we don't believe it would be a good example of how to apply this
    accessibility measure and it won't be good for education

--- modules/core_systems/settings/feature_flags.gd
+++ modules/core_systems/settings/feature_flags.gd
@@ -1,24 +1,21 @@
 @tool
 extends Node
 
-const CONFIG_FILE_PATH: StringName = "user://feature_flags.cfg"
-const DEFAULT_CONFIG_FILE_PATH: StringName = "res://modules/core_systems/settings/feature_flags.cfg"
+const CONFIG_FILE_PATH: StringName = "res://modules/core_systems/settings/feature_flags.cfg"
 var config_file: ConfigFile
 
-enum GraphicsQualityOptions { NORMAL, LOW }
-var graphics_quality: GraphicsQualityOptions = GraphicsQualityOptions.NORMAL
-
 func _ready() -> void:
-	if not FileAccess.file_exists(CONFIG_FILE_PATH):
-		print("Feature flag config file does not exist. Creating a file with the default configuration in \"%s\"" % CONFIG_FILE_PATH)
-		var default_file = FileAccess.open(DEFAULT_CONFIG_FILE_PATH, FileAccess.READ)
-		var content = default_file.get_as_text()
-		var config = FileAccess.open(CONFIG_FILE_PATH, FileAccess.WRITE)
-		config.store_string(content)
-
 	config_file = ConfigFile.new()
 	config_file.load(CONFIG_FILE_PATH)
-	load_variables()
 
-func load_variables():
-	graphics_quality = GraphicsQualityOptions.get(config_file.get_value("graphics", "quality", "NORMAL"), GraphicsQualityOptions.NORMAL)
+func get_value(section: StringName, key: StringName, default: Variant = null) -> Variant:
+	return config_file.get_value(section, key, default)
+
+func get_boolean(section: StringName, key: StringName, default: bool = false) -> bool:
+	return config_file.get_value(section, key, default) as bool
+
+func get_flag(key: StringName, default: bool = false) -> bool:
+	return get_boolean("feature_flags", key, default)
+
+func get_string(section: StringName, key: StringName, default: String = "") -> String:
+	return config_file.get_value(section, key, default) as String

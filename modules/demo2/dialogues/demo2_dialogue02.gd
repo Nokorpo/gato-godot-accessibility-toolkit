2b09a7839f23978c990ca59dd514103c0500f0cf
Edearth <nepormoseno1992@gmail.com>
2026-06-11 14:53:14 +0200

    Multiple fixes for demo 2
    - Dialogue lines that didn't fit the dialogue box
    - Remove dialogue triggers from scene after they trigger a dialogue
    - The bridge platform for the boar on an island to cross was going
      underwater
    - A floor under the water was preventing the player from dying when
      touching water near the coast
    - A typo ("tu" when it should say "su")

--- modules/demo2/dialogues/demo2_dialogue02.gd
+++ modules/demo2/dialogues/demo2_dialogue02.gd
@@ -1,9 +1,11 @@
+## This Source Code Form is subject to the terms of the Mozilla Public
+## License, v. 2.0. If a copy of the MPL was not distributed with this
+## file, You can obtain one at http://mozilla.org/MPL/2.0/.
 extends Node
 
 @export var dialogue: DialogueContainer
-var _already_loaded: bool = false
 
-func _on_gato_boar_selected(enclosure):
-	if !_already_loaded:
-		DialogueSystem.load_dialogue(dialogue)
-		_already_loaded = true
+func _on_gato_boar_selected(_enclosure):
+	await get_tree().create_timer(0.5).timeout
+	DialogueSystem.load_dialogue(dialogue)
+	queue_free()
--- modules/demo2/dialogues/demo2_dialogue03.gd
+++ modules/demo2/dialogues/demo2_dialogue03.gd
@@ -1,3 +1,6 @@
+## This Source Code Form is subject to the terms of the Mozilla Public
+## License, v. 2.0. If a copy of the MPL was not distributed with this
+## file, You can obtain one at http://mozilla.org/MPL/2.0/.
 extends Node
 
 @export var boars_node: Node3D
@@ -8,4 +11,7 @@ func _ready():
 		boar.connect("finished_growing", _activate_dialogue)
 
 func _activate_dialogue():
+	for boar:EnclosureBoar in boars_node.get_children():
+		boar.disconnect("finished_growing", _activate_dialogue)
 	DialogueSystem.load_dialogue(dialogue)
+	queue_free()

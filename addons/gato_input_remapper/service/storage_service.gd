9938e885df8bcbb8a537ff2252936dec39e16ca0
Edearth <nepormoseno1992@gmail.com>
2026-06-24 23:38:39 +0200

    More fixes for Input Remapper

--- addons/gato_input_remapper/service/storage_service.gd
+++ addons/gato_input_remapper/service/storage_service.gd
@@ -1,6 +1,7 @@
 ## This Source Code Form is subject to the terms of the Mozilla Public
 ## License, v. 2.0. If a copy of the MPL was not distributed with this
 ## file, You can obtain one at http://mozilla.org/MPL/2.0/.
+@tool
 extends RefCounted
 ## This class loads and stores control schemes and input configuration defined in InputRemapper. It
 ## stores all data in "user://input.data" by default.[br][br]
